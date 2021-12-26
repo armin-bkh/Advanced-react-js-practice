28eecd404424972cbb91313c72b570488eced169
armin-bkh <arminbkh0921@yahoo.com>
2021-12-26 20:34:16 +0330

    use useReducer instead of useState in useFetch hook

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -1,25 +1,56 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
+
+const actions = {
+  fetchRequest: "FETCH_DATA_REQUEST",
+  fetchSuccess: "FETCH_DATA_SUCCESS",
+  fetchFailure: "FETCH_DATA_FAILURE",
+};
+
+const initialState = {
+  loading: true,
+  error: null,
+  data: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actions.fetchRequest: {
+      return { ...state, loading: true, data: null, error: null };
+    }
+    case actions.fetchSuccess: {
+      return { ...state, loading: false, data: action.payload, error: null };
+    }
+    case actions.fetchFailure: {
+      return { ...state, loading: false, error: action.payload, data: null };
+    }
+    default:
+      return state;
+  }
+};
 
 const useFetch = (url) => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+  //   const [data, setData] = useState(null);
+  //   const [error, setError] = useState(null);
+  //   const [loading, setLoading] = useState(true);
+  const [data, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
+    // setLoading(true);
+    // setError(null);
+    // setData(null);
+    dispatch({ type: actions.fetchRequest });
     axios
       .get(url)
       .then((res) => {
-        setLoading(false);
-        setData(res.data);
+        dispatch({ type: actions.fetchSuccess, payload: res.data });
       })
       .catch((err) => {
-        setLoading(false);
-        setError(err.message);
+        dispatch({ type: actions.fetchFailure, payload: err.message });
       });
   }, [url]);
 
-  return { data, error, loading };
+  return data;
 };
 
 export default useFetch;
